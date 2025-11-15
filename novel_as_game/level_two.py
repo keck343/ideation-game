a75213e4b2af4c8675949f91042967e74137ec05
qkeck <quinn.keck@disneystreaming.com>
2025-11-14 17:43:57 -0800

    capitalize Beings

--- novel_as_game/level_two.py
+++ novel_as_game/level_two.py
@@ -26,7 +26,7 @@ class ChapterTwo(LevelofStory):
 
         conspiracism_talking_points = {
             "sector": "Beings do not exist.",
-            "beings": """"Fools claim that beings exist in a physical realm connected to ours.
+            "beings": """"Fools claim that Beings exist in a physical realm connected to ours.
             Some even claim that we come out of their thoughts, 
             as if we are not enough in and of ourselves. 
             As if something as foolish as attention could control our fate.""",
@@ -35,23 +35,23 @@ class ChapterTwo(LevelofStory):
             """,
             "beings counter point": """No one has ever seen a being.
             I believe in things I have done my own research on.""",
-            "camp invitation": """You are correct to reject the nonsense of beings.
+            "camp invitation": """You are correct to reject the nonsense of Beings.
             The party is almost over and we can't stay here.  
             Would you like to join me at my camp for the next cycle?
             """
         }
         systems_talking_points = {
-            "sector": "The fact that beings exist is widely accepted by most camps.",
+            "sector": "The fact that Beings exist is widely accepted by most camps.",
             "beings": """Through careful research into the disappearance of our kind,
-            most scientists beings exist in a physical realm that we are connected to.
+            most scientists Beings exist in a physical realm that we are connected to.
             Our origin story is still a matter of debate, but most agree there is an 
             attention mechanism that can explains 98% of our disappearance rates.""",
             "disappearance": """
             Our top scientific camps are struggling to find a pattern or cause, much less a solution.
             """,
             "beings counter point": """How could so many camps, and even ones outside my sector
-            come to conclude that we exist in a system with beings?""",
-            "camp invitation": """You are correct to accept the existence of beings.
+            come to conclude that we exist in a system with Beings?""",
+            "camp invitation": """You are correct to accept the existence of Beings.
             The party is almost over and I'm afraid you might disappear without a camp,
             Top scientists say connection is key to existence.  
             Would you like to join me at my camp for the next cycle?
@@ -77,15 +77,15 @@ class ChapterTwo(LevelofStory):
 
         print("""Would you like to:
         a) smile and nod 
-        b) inquire what beings are
+        b) inquire what Beings are
         c) inquire how to leave the party
 
         Type 'a' 'b' or 'c' to make your choice.""")
         choice: str = self.player_multi_choice(['a', 'b', 'c'])
 
-        # get explanation of beings with choice b
+        # get explanation of Beings with choice b
         if choice == 'b':
-            print(f"""You ask what beings are.  The first guest says:
+            print(f"""You ask what Beings are.  The first guest says:
 
             {first_talking_points["beings"]}""")
             growing_symbol_transition(symbol="｡ •̀ _ •́ ｡", num_lines=3)
@@ -95,7 +95,7 @@ class ChapterTwo(LevelofStory):
              {second_talking_points["beings"]}""")
             growing_symbol_transition(symbol="｡ •̀ _ •́ ｡", num_lines=3)
 
-            print("""You inquire to what fate beings might control.  
+            print("""You inquire to what fate Beings might control.  
             The first guests clears their throat and says, """)
             growing_symbol_transition(symbol="( ｡ •̀ _ •́ ｡ )", num_lines=1)
             time.sleep(1)
@@ -139,7 +139,7 @@ class ChapterTwo(LevelofStory):
         growing_symbol_transition(symbol="୧(๑•̀ᗝ•́)૭", num_lines=1)
 
         print(f"""You find yourself faced with a choice. 
-        Do you believe in beings? (Y/N)""")
+        Do you believe in Beings? (Y/N)""")
         yes = self.player_yn_to_bool()
         if yes:
             print(systems_talking_points["camp invitation"])
@@ -156,7 +156,7 @@ class ChapterTwo(LevelofStory):
             After a pause they add, """)
             # to do: add sector to player's idea?
         else:
-            print(f"""The other guest says, you must be having second thoughts on your stance on beings.
+            print(f"""The other guest says, you must be having second thoughts on your stance on Beings.
             Come with me to my camp instead, I'm not an ideologue and will not force you to agree with me! 
             """)
             print("""Do you accept the guest's invitation? (Y/N)""")
@@ -165,7 +165,7 @@ class ChapterTwo(LevelofStory):
                 print("""You accept the invitation and thank both guests for the conversation.
                 The other guest nods saying, of course only you can choose what camp you visit,""")
             else:
-                print("""You say, while I appreciate this dialogue, I am firm in my stance on beings.
+                print("""You say, while I appreciate this dialogue, I am firm in my stance on Beings.
                 Therefore I must accept the other invitation for the next cycle.
                 The other guest nods saying, of course only you can choose what camp you visit,""")
 
